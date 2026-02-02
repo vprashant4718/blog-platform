a70b5b11c23d8464845bc0241739f755e9f293e2
Prashant Verma <vprashant4718@gmail.com>
2026-02-02 15:05:11 +0530

    update & create: cors origin and created vercel.jso for deployment

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: true,
+    origin: process.env.FRONTEND_URL,
     credentials: true,
 }))
 mongoose.connect(MONGO).then(()=>{
