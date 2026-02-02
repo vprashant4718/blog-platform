61cb3fe3670a9aa6aaeaff6192d565a284740977
Prashant Verma <vprashant4718@gmail.com>
2026-02-02 15:19:42 +0530

    update: fetch api url admin panel

--- admin-panel/src/utils/api.ts
+++ admin-panel/src/utils/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://blog-platform-delta-lovat.vercel.app/api",
   withCredentials: true // this is the key line
 });
 
